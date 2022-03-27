3010c982d8abb5dea18fb5d85743d0f793aa0af3
josegallego21 <95502617+josegallego21@users.noreply.github.com>
2022-03-27 12:07:34 -0500

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HelloComponent } from './hello/hello.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";  
+
+@Component({
+    selector: 'custom-hello',
+    templateUrl: './hello.component.html',
+    styleUrls: [
+        './hello.component.css'
+    ]
+})
+export class HelloComponent{
+    @Input() name:string;
+    
+    constructor(){
+    }
+ 
+}
